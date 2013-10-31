ed4c42d258b3537ddd4daf9e4a5b1c8bd8645ef1
oulrich1 <oemulrich@gmail.com>
2013-10-30 19:53:29 -0700

    wrote test data in seeds.rb

--- doi_server/db/seeds.rb
+++ doi_server/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create(email: "o2@g.com", password: "password", sign_in_count: 1)
+doi = Doi.create(label: "doi-test", description: "this doi was created in seeds.rb", user_id: user.id)
+url = Url.create(doi_id: doi.id, url_text: "http://www.google.com")
+comment = Comment.create(comment: "this is a comment and its about a doi", doi_id: doi.id, user_id: user.id)
